71bbb9406f4e272accc34491e3567b5fd8ef1226
Antonio Garcia <oitog3@gmail.com>
2026-06-11 04:24:14 -0600

    modificacion .js

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-app.use(cors());
-
-app.use(express.json());
-
-const cors = require('cors');
-
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const { Pool } = require('pg');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // Configuración de PostgreSQL
